30a0d0c42bd30f77e43c7ba251ef32fa6233f260
iaanachironran <zhenyou905867096@126.com>
2021-06-15 15:46:12 +0800

    菜单

--- app/common/controller/Commonbase.php
+++ app/common/controller/Commonbase.php
@@ -9,7 +9,7 @@ class Commonbase extends Controller
 		parent::__construct();
 
         // 网站名称设置
-        $this->web_title = '小小图书';
+        $this->web_title = '艾莫森通航';
         $this->assign('web_title', $this->web_title);
     }
 }
